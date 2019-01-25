332782966c13b0320e549ea95b3db1aa49da484f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-01-25 11:10:21 -0500

    Add the starting version of test_compute_tf_cookbook with one test.

---
+++ tests/test_compute_tf_cookbook.py
@@ -0,0 +1,11 @@
+"""Test cases for the comments and entities modules"""
+
+from termfrequency import compute_tf_cookbook
+
+
+def test_read_file_populates_data():
+    """Checks that the singleline comment count works"""
+    # pylint: disable=len-as-condition
+    assert len(compute_tf_cookbook.data) == 0
+    compute_tf_cookbook.read_file("inputs/input.txt")
+    assert len(compute_tf_cookbook.data) != 0
